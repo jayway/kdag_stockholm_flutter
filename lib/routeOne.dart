7d9e960e406bdf6ccfae079f37afa35e19a99cc4
Astr Magnussen <dic12ama@student.lu.se>
2019-01-24 11:07:59 +0100

    added item view

--- lib/routeOne.dart
+++ lib/routeOne.dart
@@ -4,7 +4,27 @@ class RouteOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('One'),
+      body: createListItemView('assets/flag.png', 'UK'),
     );
+  }
+
+  Widget createListItemView(String countryImgPath, String countryName) {
+    return Container(
+        margin: EdgeInsets.only(left: 16, top: 16, bottom: 16, right: 16),
+        child: Row(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.only(right: 32),
+                child: Image(
+                  height: 80,
+                  width: 80,
+                  image: AssetImage(countryImgPath),
+                )),
+            Text(
+              countryName,
+              style: TextStyle(fontSize: 20, color: Colors.blueGrey),
+            )
+          ],
+        ));
   }
 }
